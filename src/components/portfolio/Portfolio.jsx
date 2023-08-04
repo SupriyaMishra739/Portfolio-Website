a3f0884d59ccdf8ec4120e6c2298e715e31e49d7
Supriya Mishra <supriya.mishra@nineleaps.com>
2023-08-04 11:30:34 +0530

    (Feat): Added the features for the experience,services and the portfolio sections

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import './portfolio.css'
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio website</section>
+    <section id='portfolio'>
+
+
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+      <article className='portfolio__item'>
+
+
+
+
+      </article>
+    </section>
   )
 }
 
